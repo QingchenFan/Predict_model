a43ffb9c78c18b7ad244c186bc171d513dd12ac3
fanqingchen <fqc_nwu@163.com>
2023-02-17 15:29:06 +0800

    fix

--- FC_PLSR_Prediction_PFactor/step1_PLSr/ToolBox.py
+++ FC_PLSR_Prediction_PFactor/step1_PLSr/ToolBox.py
@@ -54,22 +54,22 @@ def countnum_2():
     f_out.close()
     return num
 
-def send_result_Ding(test):
-    webhook = 'https://oapi.dingtalk.com/robot/send?access_token=36aecb847d993acde01b431d66e178e66b54b1aacf4013747c930aa621ec7e9e'
-    xiaoding = DingtalkChatbot(webhook)
-    Res = 'Result:'+'\n' + test
-    xiaoding.send_text(msg=Res,is_at_all=False)
-def send_warning_Ding(test):
-    webhook = 'https://oapi.dingtalk.com/robot/send?access_token=03d69526107ebfbb9124815833826ad2e3967307d9783bc23d1ba05ab340e6d3'
-    xiaoding = DingtalkChatbot(webhook)
-    Res = 'Warning:'+'\n' + str(test)
-    xiaoding.send_text(msg=Res,is_at_all=False)
-'''
-    curl 'https://oapi.dingtalk.com/robot/send?access_token=03d69526107ebfbb9124815833826ad2e3967307d9783bc23d1ba05ab340e6d3' \
-    - H 'Content-Type: application/json' \
-    - d '{"msgtype": "text",
-           "text": {
-            "content": "Warning: 钉钉机器人群消息测试"
-             }
-          }'
-'''
+# def send_result_Ding(test):
+#     webhook = 'https://oapi.dingtalk.com/robot/send?access_token=36aecb847d993acde01b431d66e178e66b54b1aacf4013747c930aa621ec7e9e'
+#     xiaoding = DingtalkChatbot(webhook)
+#     Res = 'Result:'+'\n' + test
+#     xiaoding.send_text(msg=Res,is_at_all=False)
+# def send_warning_Ding(test):
+#     webhook = 'https://oapi.dingtalk.com/robot/send?access_token=03d69526107ebfbb9124815833826ad2e3967307d9783bc23d1ba05ab340e6d3'
+#     xiaoding = DingtalkChatbot(webhook)
+#     Res = 'Warning:'+'\n' + str(test)
+#     xiaoding.send_text(msg=Res,is_at_all=False)
+# '''
+#     curl 'https://oapi.dingtalk.com/robot/send?access_token=03d69526107ebfbb9124815833826ad2e3967307d9783bc23d1ba05ab340e6d3' \
+#     - H 'Content-Type: application/json' \
+#     - d '{"msgtype": "text",
+#            "text": {
+#             "content": "Warning: 钉钉机器人群消息测试"
+#              }
+#           }'
+# '''
